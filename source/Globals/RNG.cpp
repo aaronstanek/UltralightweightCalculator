058528d33f7249e2f601cfcb320545d8a931bfa5
Aaron Stanek <amr.stanek@gmail.com>
2021-04-05 13:18:14 -0700

    Documented RNG

--- source/Globals/RNG.cpp
+++ source/Globals/RNG.cpp
@@ -5,11 +5,15 @@
 #include "RNG.h"
 #include "../LowLevelConvert/LowLevelConvert.h"
 
+/// A class to hold the state of
+/// a pseudorandom number generator.
 struct RNG_State_Struct {
     unsigned long A, B;
+    /// Assigns A and B
+    /// based on the current time.
+    /// This may have implementation-defined behavior.
     RNG_State_Struct() noexcept {
         const long long t = difftime(time(NULL),0);
-        // value of t may be implementation-defined
         // t is the current time to whatever precision
         // we are able to get
         A = t & 0xFFFFFFFF;
@@ -17,8 +21,16 @@ struct RNG_State_Struct {
     }
 };
 
+/// The current state of the program's
+/// pseudorandom number generator.
 RNG_State_Struct RNG_State;
 
+/// Increments the state of the program's
+/// pseudorandom number generator.
+/// Then returns a pseudorandom number.
+/// @return A nonnegative integer.
+/// The least significant 32 bits of which are uniformly
+/// distributed across the range 0 <= value < (2^32).
 unsigned long RNG_getRaw() noexcept {
     if (RNG_State.B >= MAX_UNSIGNED_INTEGER_VALUE) {
         RNG_State.B = 0;
@@ -45,6 +57,15 @@ unsigned long RNG_getRaw() noexcept {
     return A;
 }
 
+/// Increments the state of the program's
+/// pseudorandom number generator.
+/// Then returns a pseudorandom number.
+/// @param minValue the smallest desired result
+/// @param maxValue the largest desired result
+/// @return An integer in the range minValue <= value <= maxValue.
+/// The result will be uniformly distributed over this range.
+/// @warning The following inequality must hold:\n
+/// -( 2^31 - 1 ) <= minValue <= maxValue <= ( 2^31 - 1 )
 long RNG_getInt(const long minValue, const long maxValue) noexcept {
     // safe to assume that minValue <= maxValue
     const unsigned long uMinValue = convertSignedToUnsigned(minValue);
